f7cebf404e1d6a7b8fc023b3b077190c4d31e5f5
Evgeniy Kochu <evgeniykochu@gmail.com>
2021-12-13 20:18:34 +0300

    feat: full page layout

---
+++ src/index.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+	console.dir(document.querySelector('.amount__number'));
+	//changeNumberTickets();
+	//let amountNumber = document.querySelector('.amount__number');
+
+}
+
+/*const changeNumberTickets = document.querySelector('.amount__up').addEventListener('click', function () {
+	//console.log();
+	let input = document.querySelector('.amount__number');
+	input.stepUp();
+})*/
