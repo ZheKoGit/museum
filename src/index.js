d533536dad879cd9ab4d87ed088cc4d6571bf448
Evgeniy Kochu <evgeniykochu@gmail.com>
2022-01-09 20:01:55 +0300

    feat: styling blocks Video

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 window.onload = () => {
-	console.dir(document.querySelector('.amount__number'));
+	//console.dir(document.querySelector('.amount__number'));
 	//changeNumberTickets();
 	//let amountNumber = document.querySelector('.amount__number');
-
+	//console.log(document.querySelector('.play-range'));
+	changeStylePlayerRange();
 }
 
 /*const changeNumberTickets = document.querySelector('.amount__up').addEventListener('click', function () {
@@ -10,3 +11,14 @@ window.onload = () => {
 	let input = document.querySelector('.amount__number');
 	input.stepUp();
 })*/
+
+function changeStylePlayerRange() {
+	const playerControl = document.querySelector('.player__control');
+	playerControl.addEventListener('input', (e) => {
+		if (e.target.classList.contains('player__control-range')) {
+			console.log(e.target.value);
+			e.target.style.background = `linear-gradient(to right, #710707 ${e.target.value}%, #C4C4C4 ${e.target.value}%)`;
+		}
+	})
+	//console.dir(playerControl);
+}
