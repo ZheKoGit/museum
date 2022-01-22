cfe3d1916a45b7be84ba2a9dd47cce55f1de0012
Evgeniy Kochu <evgeniykochu@gmail.com>
2022-01-22 16:25:54 +0300

    feat: styling blocks Video, Gallery

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ window.onload = () => {
 	//let amountNumber = document.querySelector('.amount__number');
 	//console.log(document.querySelector('.play-range'));
 	changeStylePlayerRange();
+	startPlayer();
 }
 
 /*const changeNumberTickets = document.querySelector('.amount__up').addEventListener('click', function () {
@@ -21,4 +22,19 @@ function changeStylePlayerRange() {
 		}
 	})
 	//console.dir(playerControl);
+}
+
+function startPlayer() {
+	const player = document.querySelector('.player-wrapper');
+	const playerInner = player.querySelector('.player-inner');
+	const playerLink = player.querySelector('.player-link');
+	const playerPlaySizeBig = player.querySelector('.player__play_size_big');
+	playerInner.addEventListener('click', () => {
+		playerLink.classList.add('player-link_hidden');
+		playerPlaySizeBig.classList.add('player__play_size_big_hidden');
+		//const playerIframe = player.createElement('iframe');
+		const playerIframe = '<iframe class="player__iframe" width="1440" src="https://www.youtube.com/embed/zp1BXPX8jcU?controls=0&autoplay=1&modestbranding=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+		playerInner.innerHTML = playerIframe;
+		console.log(playerPlaySizeBig.style.display);
+	})
 }
